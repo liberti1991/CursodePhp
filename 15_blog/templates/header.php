db5ab4f93d7f9dd0ed9ecb84047e0340642b45dc
Liberti1991 <liberti1991@gmail.com>
2022-10-12 11:10:44 -0300

    ajustes layout

--- 15_blog/templates/header.php
+++ 15_blog/templates/header.php
@@ -3,17 +3,18 @@ include_once("helpers/url.php");
 include_once("data/posts.php");
 include_once("data/categories.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+  <title>Blog Codar</title>
+  <!-- ESTILOS DO PROJETO -->
   <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
-  <title>Document</title>
+  <!-- GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -23,18 +24,10 @@ include_once("data/categories.php");
     </a>
     <nav>
       <ul id="navbar">
-        <li>
-          <a href="<?= $BASE_URL ?>">HOME</a>
-        </li>
-        <li>
-          <a href="#">Categorias</a>
-        </li>
-        <li>
-          <a href="3">Sobre</a>
-        </li>
-        <li>
-          <a href="<?= $BASE_URL ?>contato.php">contato</a>
-        </li>
+        <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+        <li><a href="#" class="nav-link">Categorias</a></li>
+        <li><a href="#" class="nav-link">Sobre</a></li>
+        <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
       </ul>
     </nav>
   </header>
