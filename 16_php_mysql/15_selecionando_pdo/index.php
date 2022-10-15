1455ab0f888d09c4a9e91892819643ea177cf705
Liberti1991 <liberti1991@gmail.com>
2022-10-15 17:56:06 -0300

    finalizando a seção de msqli com pdo no php

---
+++ 16_php_mysql/15_selecionando_pdo/index.php
@@ -0,0 +1,27 @@
+<?php
+
+  $host = "localhost";
+  $db = "cursophp";
+  $user = "root";
+  $pass = "";
+
+  $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+  // ASSUNTO DA AULA
+  $id = 5;
+
+  $stmt = $conn->prepare("SELECT * FROM itens WHERE id > :id");
+
+  $stmt->bindParam(":id", $id);
+
+  $stmt->execute();
+
+  $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  print_r($data);
+
+  echo "<br><br>";
+
+  $itens = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  print_r($itens);
